d930930ad09a4e7f6c64b209fd7dc26d90584777
Mars <knownasmars@gmail.com>
2023-05-20 14:04:58 +0300

    5. Повествование основанное на хореографии [#458498]

--- src/main/java/ru/job4j/kitchen/service/KitchenService.java
+++ src/main/java/ru/job4j/kitchen/service/KitchenService.java
@@ -1,15 +1,29 @@
 package ru.job4j.kitchen.service;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import ru.job4j.kitchen.model.Order;
 
 @Service
 @Slf4j
+@AllArgsConstructor
 public class KitchenService {
-    @KafkaListener(topics = "job4j_orders")
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @KafkaListener(topics = "preorder")
     public void receiveOrder(Order order) {
         log.debug(order.toString());
+        kafkaTemplate.send("cooked_order", getDish(order.getDishId()));
+    }
+
+    private String getDish(int dishId) {
+        if (dishId % 2 == 0) {
+            return "Готов к выдаче";
+        }
+        return "Нет такого блюда";
     }
 }
